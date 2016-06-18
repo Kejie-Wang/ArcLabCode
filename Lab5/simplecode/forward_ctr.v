b8b767bc2ee060a33b884b362e8562a9b70ede33
JackWang822 <wang_kejie@foxmail.com>
2016-06-18 19:31:48 +0800

    update the forward_ctr

--- Lab5/simplecode/forward_ctr.v
+++ Lab5/simplecode/forward_ctr.v
@@ -22,8 +22,6 @@
 //macro definitions
 
 //operation code
-`include "macro.vh"
-
 module forward_ctr(clk,
 						rst,
 						id_inA,
@@ -31,35 +29,65 @@ module forward_ctr(clk,
 						
 						id_rs,
 						id_rt,
-						ex_wreg,
-						ex_destR,
-						ex_aluR,
-						 
+						
+						mem_wreg,
+						mem_destR,
+						mem_aluR,
+						
+						wb_wreg,
+						wb_destR,
+						wb_dest,
+						
 						id_inA_forward,
 						id_inB_forward,
+						
+						inst,
+						if_inst,
 						stall
 						);
 		
 		input rst, clk;
 		input [31:0]inst;
 		input [31:0]if_inst;
-		input [31:0]id_inst;
+		input [31:0]id_inA;
+		input [31:0]id_inB;
+		input [4:0]id_rs;
+		input [4:0]id_rt;
+		input mem_wreg;
+		input [4:0]mem_destR;
+		input [31:0]mem_aluR;
+		input wb_wreg;
+		input [4:0]wb_destR;
+		input [31:0]wb_dest;
 		
-		output id_inA_forward, id_inB_forward;
+		output [31:0]id_inA_forward;
+		output [31:0]id_inB_forward;
 		output reg stall;
 		
-		wire AfromEx, BfromEx, AfromExLW, BfromExLW;
-		wire AfromMem, BfromMem, AfromMemLW, BfromMemLW;
+		wire [4:0]rs;
+		wire [4:0]rt;
+		wire [5:0]if_op;
+		wire [4:0]if_rt;
 		
-		assign id_inA_forward = (ex_wreg && (exdestR != 0) && (exdestR == id_rs)) ? ex_aluR : id_inA;
-		assign id_inB_forward = (ex_wreg && (exdestR != 0) && (exdestR == id_rt)) ? ex_aluR : id_inB;
+		assign rs = inst[25:21];
+		assign rt = inst[20:16];
+		assign if_op = if_inst[31:26];
+		assign if_rt = if_inst[20:16];
+
+		wire wb_rs_forward, wb_rt_forward, mem_rs_forward, mem_rt_forward;
+		assign mem_rs_forward = (mem_wreg && (mem_destR != 0) && (mem_destR == id_rs));
+		assign mem_rt_forward = (mem_wreg && (mem_destR != 0) && (mem_destR == id_rt));
+		assign wb_rs_forward = (wb_wreg && (wb_destR != 0) && (wb_destR == id_rs));
+		assign wb_rt_forward = (wb_wreg && (wb_destR != 0) && (wb_destR == id_rt));
 		
-		
-		
+		assign id_inA_forward = (mem_rs_forward) ? mem_aluR : ((wb_rs_forward) ? wb_dest : id_inA);
+		assign id_inB_forward = (mem_rt_forward) ? mem_aluR : ((wb_rt_forward) ? wb_dest : id_inB);
+	
 		always @(*) begin
 			if(rst)
 				stall <=0;
 			else
-				stall <= AfromEx | BfromEx |AfromExLW | BfromExLW | AfromMem | BfromMem | AfromMemLW | BfromMemLW;
+				stall <= ((if_op == 6'b100011) && ((if_rt == rs) || (if_rt == rt))) ? 1 : 0;
 		end
+		
 endmodule
